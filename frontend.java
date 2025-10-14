ed82e8e9afa88b24a7be642049008b86364e58c5
Chethan-m <chethanmshetty19@gmail.com>
2025-10-14 18:34:55 +0000

    modified frontend.java file in master branch

--- frontend.java
+++ frontend.java
@@ -1,8 +1,8 @@
 Hello,
 	This file contains frontend code.
-<<<<<<< HEAD
+
 	Am Chethan from master branch.
 
-=======
+
         This is Chethan am a DevOps Engineer. I have modified the code from development branch.
->>>>>>> development
+ development
