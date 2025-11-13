57cfc6d42c3c8e958078e308c2b637a9cfd5e93b
Chethan-m <chethanmshetty19@gmail.com>
2025-11-13 13:59:03 +0000

    modified frontend.java file in stage branch while working on stash command

--- frontend.java
+++ frontend.java
@@ -6,3 +6,5 @@ Hello,
 
         This is Chethan am a DevOps Engineer. I have modified the code from development branch.
  development
+
+ Am working on frontend.java file in stagging branch, testing on stash command in stagging branch
